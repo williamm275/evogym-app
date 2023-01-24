8c237331383a934335b11b90965206be625c6bcd
William Martinez <wrmartinez709@gmail.com>
2023-01-24 18:04:33 -0500

    Remove extra top margin on mobile view

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -23,7 +23,7 @@ const Home = ({ setSelectedPage }: Props) => {
         onViewportEnter={() => setSelectedPage(SelectedPage.Home)}
       >
         {/* MAIN HEADER */}
-        <div className="z-10 mt-32 md:basis-3/5">
+        <div className="z-10 md:basis-3/5">
           {/* HEADINGS */}
           <motion.div
             className="md:-mt-20"
